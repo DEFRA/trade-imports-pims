f860ebafdf81fa87e7596cbc6aaef96be7411193
O'Leary, Aidan <aidan.o-leary@capgemini.com>
2019-10-04 09:03:23 +0100

    Updated autonumber functionality

---
+++ Solutions/DefraImportsWorkflows/WorkflowAssemblies/Defra.Imports.Workflows/WorkflowActivity.cs
@@ -0,0 +1,31 @@
+﻿using Defra.Imports.BusinessLogic.Logging;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defra.Imports.Workflows
+{
+    /// <summary>
+    /// Base class for Dynamics 365 workflow activities.
+    /// </summary>
+    public abstract class WorkflowActivity : CodeActivity
+    {
+        internal abstract void ExecuteWorkflowActivity(CodeActivityContext context, IWorkflowContext workflowContext, IOrganizationService orgSvc, ILogWriter logWriter);
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var tracingSvc = context.GetExtension<ITracingService>();
+            var workflowContext = context.GetExtension<IWorkflowContext>();
+            var serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            var orgSvc = serviceFactory.CreateOrganizationService(workflowContext.UserId);
+            var logWriter = new TracingServiceLogWriter(tracingSvc);
+
+            this.ExecuteWorkflowActivity(context, workflowContext, orgSvc, logWriter);
+        }
+    }
+}
---
+++ Tests/Defra.Imports.Tests.Unit/Workflows/Autonumber/ResetAutonumberTests.cs
@@ -0,0 +1,47 @@
+﻿using Defra.Imports.Model;
+using Defra.Imports.Workflows.Autonumber;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Defra.Imports.Tests.Unit.Workflows.Autonumber
+{
+    public class ResetAutonumberTests : WorkflowActivityTests<ResetAutonumber>
+    {
+        [Fact]
+        public void Execute_WithAValidName_ShouldResetTheAutoNumbersCounterToZero()
+        {
+            // Arrange
+            defraimp_autonumber configEntityStub = new defraimp_autonumber()
+            {
+                defraimp_CurrentNumber = 10
+            };
+            EntityCollection entityColStub = new EntityCollection();
+            entityColStub.Entities.Add(configEntityStub);
+
+            // Act
+            this.OrgSvcMock.Setup(o => o.RetrieveMultiple(It.IsAny<QueryExpression>())).Returns(() => {
+                return entityColStub;
+            });
+
+            this.WorkflowInvoker.Invoke(this.GetInputs("test_autonumber_record"));
+
+            // Assert
+            Assert.Equal(configEntityStub.defraimp_CurrentNumber, 0);
+        }
+
+        private IDictionary<string, object> GetInputs(string keyName)
+        {
+            return new Dictionary<string, object>()
+            {
+                { "AutonumberCounterName", keyName },
+            };
+        }
+    }
+}
