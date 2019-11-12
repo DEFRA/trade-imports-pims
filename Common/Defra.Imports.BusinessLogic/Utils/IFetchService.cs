09f6a6e7e8354113eb9b52c5a2bdea222045ac2f
O'Leary, Aidan <aidan.o-leary@capgemini.com>
2019-11-12 15:20:25 +0000

    Added logic for CountRecordsByFetchXml

---
+++ Common/Defra.Imports.BusinessLogic/Utils/IFetchService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Defra.Imports.BusinessLogic.Utils
+{
+    using System.Collections.Generic;
+    using Microsoft.Xrm.Sdk;
+
+    public interface IFetchService
+    {
+        IEnumerable<Entity> FetchByTemplate(string templateFetchXml, EntityReference primaryEntity, Dictionary<string, object> additionalValues = null);
+
+        IEnumerable<TEntity> FetchByTemplate<TEntity>(string templateFetchXml, EntityReference primaryEntity, Dictionary<string, object> additionalValues = null)
+            where TEntity : Entity;
+    }
+}
---
+++ Common/Defra.Imports.BusinessLogic/Utils/IFetchTemplateParser.cs
@@ -0,0 +1,10 @@
+﻿namespace Defra.Imports.BusinessLogic.Utils
+{
+    using System.Collections.Generic;
+    using Microsoft.Xrm.Sdk;
+
+    public interface IFetchTemplateParser
+    {
+        string Parse(string fetchXmlTemplate, EntityReference primaryEntity, IDictionary<string, object> additionalValues = null);
+    }
+}
