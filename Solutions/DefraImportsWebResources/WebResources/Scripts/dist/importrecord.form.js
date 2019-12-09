c22db88dc876f4dbc37ce5e023f7d1caa2c52dc2
Mike Andrews <mike.andrews@capgemini.com>
2019-12-09 11:19:03 +0000

    Added JS to clear the owner from the quick create form for import record.

---
+++ Solutions/DefraImportsWebResources/WebResources/Scripts/dist/importrecord.form.js
@@ -0,0 +1,14 @@
+var DefraImports;
+(function (DefraImports) {
+    var ImportRecord;
+    (function (ImportRecord) {
+        function OnLoadQuickCreateForm(executionObj) {
+            var formContext = executionObj.getFormContext();
+            if (formContext.ui.getFormType() === 1 /* Create */) {
+                formContext.getAttribute("ownerid").setValue(null);
+            }
+        }
+        ImportRecord.OnLoadQuickCreateForm = OnLoadQuickCreateForm;
+    })(ImportRecord = DefraImports.ImportRecord || (DefraImports.ImportRecord = {}));
+})(DefraImports || (DefraImports = {}));
+//# sourceMappingURL=importrecord.form.js.map
