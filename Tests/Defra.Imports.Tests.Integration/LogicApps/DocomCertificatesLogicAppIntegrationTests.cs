08891e2b859c3357e16ef965edaa6328d2be1b37
O'Leary, Aidan <aidan.o-leary@capgemini.com>
2020-03-04 16:43:58 +0000

    Merged PR 9757: Refactored certificate logic app tests
    Refactored certificate logic app tests

---
+++ Tests/Defra.Imports.Tests.Integration/LogicApps/DocomCertificatesLogicAppIntegrationTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Defra.Imports.Tests.Integration.LogicApps
+{
+    public class DocomCertificatesLogicAppIntegrationTests : CertificatesLogicAppIntegrationTestsBase
+    {
+        [Fact]
+        [ExcludeFromCodeCoverage]
+        public void SendToSBQueue_AValidDocomXMLMessage_DOCOMIsCreatedInDynamics()
+        {
+            // Arrange
+            string certificateReferenceNumber = "DOCOM.CZ.2016.0001882";
+            string docomXmlMessage = GetDocomXml(certificateReferenceNumber);
+
+            // Act
+            this.SendServiceBusMessage(docomXmlMessage);
+            Thread.Sleep(150000);
+
+            // Assert
+            DataCollection<Entity> retrievedDocoms = GetCertificatesByReferenceNumbers("defraimp_docom", certificateReferenceNumber, Array.Empty<string>());
+            Assert.True(retrievedDocoms.Count > 0);
+
+            // Clear Down
+            ClearDownCertificateTest("docom", retrievedDocoms);
+
+        }
+
+        [Fact(Skip = "This is skipped as we don't want to flood our system with records")]
+        [ExcludeFromCodeCoverage]
+        public void SendToSBQueue_ListOfDocoms_ListOfDocomsAreCreated()
+        {
+            List<string> docomXmlList = GetDocomListXml();
+
+            docomXmlList.ForEach(item => this.SendServiceBusMessage(item));
+            Thread.Sleep(300000);
+
+            // Assert
+            List<string> certificateReferenceNumbers = GetCertificateReferenceNumbersFromXml(docomXmlList);
+            DataCollection<Entity> retrievedDocoms = GetCertificatesByReferenceNumbers("defraimp_docom", certificateReferenceNumbers.ToArray(), Array.Empty<string>());
+            Assert.True(retrievedDocoms.Count == 50);
+
+            // Clear Down
+            ClearDownCertificateTest("docom", retrievedDocoms);
+        }
+
+        private string GetDocomXml(string certificateReferenceNumber)
+        {
+            return GetTestCertificateXml("DOCOM1.xml", certificateReferenceNumber);
+        }
+
+        private List<string> GetDocomListXml()
+        {
+            return GetCertificateXmlList("DOCOM_LIST.xml", "ns2:doCom");
+        }
+    }
+}
---
+++ Tests/Defra.Imports.Tests.Integration/LogicApps/ItahcCertificatesLogicAppIntegrationTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Defra.Imports.Tests.Integration.LogicApps
+{
+    public class ItahcCertificatesLogicAppIntegrationTests : CertificatesLogicAppIntegrationTestsBase
+    {
+        [Fact]
+        [ExcludeFromCodeCoverage]
+        public void SendToSBQueue_AValidItahcXMLMessage_ItahcIsCreatedInDynamics()
+        {
+            // Arrange
+            string certificateReferenceNumber = "INTRA.CZ.2019.0019190";
+            string itahcXmlMessage = GetItahcXml(certificateReferenceNumber);
+
+            // Act
+            this.SendServiceBusMessage(itahcXmlMessage);
+            Thread.Sleep(150000);
+
+            // Assert
+            DataCollection<Entity> retrievedItahcs = GetCertificatesByReferenceNumbers("defraimp_itahc", certificateReferenceNumber, Array.Empty<string>());
+            Assert.True(retrievedItahcs.Count > 0);
+
+            // Clear Down
+            ClearDownCertificateTest("itahc", retrievedItahcs);
+        }
+
+        [Fact]
+        [ExcludeFromCodeCoverage]
+        public void SendToSBQueue_ItahcWithIdentificationParameters_ItahcIsCreatedWithIdentificationParameters()
+        {
+            // Arrange
+            string certificateReferenceNumber = "INTRA.CZ.2019.0019190";
+            string itahcXmlMessage = GetItahcXml(certificateReferenceNumber);
+
+            // Act
+            this.SendServiceBusMessage(itahcXmlMessage);
+            Thread.Sleep(150000);
+
+            // Assert
+            DataCollection<Entity> retrievedItahcs = GetCertificatesByReferenceNumbers("defraimp_itahc", certificateReferenceNumber, new string[] { "defraimp_identificationofanimalstext" });
+            Assert.True(retrievedItahcs.Count > 0);
+            Assert.True(retrievedItahcs.First().Attributes.Contains("defraimp_identificationofanimalstext"));
+
+            ClearDownCertificateTest("itahc", retrievedItahcs);
+        }
+
+        [Fact]
+        [ExcludeFromCodeCoverage]
+        public void SendToSBQueue_ItahcWithCommodityComplements_ItahcIsCreatedWithCommodityComplements()
+        {
+            // Arrange
+            string certificateReferenceNumber = "INTRA.CZ.2019.0019190";
+            string itahcXmlMessage = GetItahcXml(certificateReferenceNumber);
+
+            // Act
+            this.SendServiceBusMessage(itahcXmlMessage);
+            Thread.Sleep(150000);
+
+            // Assert
+            DataCollection<Entity> retrievedItahcs = GetCertificatesByReferenceNumbers("defraimp_itahc", certificateReferenceNumber, new string[] { "defraimp_commoditycomplementstext" });
+            Assert.True(retrievedItahcs.Count > 0);
+            Assert.True(retrievedItahcs.First().Attributes.Contains("defraimp_commoditycomplementstext"));
+
+            ClearDownCertificateTest("itahc", retrievedItahcs);
+        }
+
+        [Fact(Skip = "This is skipped as we don't want to flood our system with records")]
+        [ExcludeFromCodeCoverage]
+        public void SendToSBQueue_ListOfItahcs_ListOfItahcsAreCreated()
+        {
+            // Arrange
+            List<string> itahcXmlList = GetItahcListXml();
+
+            // Act
+            itahcXmlList.ForEach(item => this.SendServiceBusMessage(item));
+            Thread.Sleep(300000);
+
+            // Assert
+            List<string> certificateReferenceNumbers = GetCertificateReferenceNumbersFromXml(itahcXmlList);
+            DataCollection<Entity> retrievedItahcs = GetCertificatesByReferenceNumbers("defraimp_itahc", certificateReferenceNumbers.ToArray(), Array.Empty<string>());
+            Assert.True(retrievedItahcs.Count == 48);
+
+            // Clear Down
+            ClearDownCertificateTest("itahc", retrievedItahcs);
+        }
+
+        private string GetItahcXml(string certificateReferenceNumber)
+        {
+            return GetTestCertificateXml("ITAHC1.xml", certificateReferenceNumber);
+        }
+
+        private List<string> GetItahcListXml()
+        {
+            return GetCertificateXmlList("ITAHC_LIST.xml", "ns2:intraTrade");
+        }
+    }
+}
