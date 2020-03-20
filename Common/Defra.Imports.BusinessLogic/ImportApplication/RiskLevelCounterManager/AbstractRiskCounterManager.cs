d8cce368ae9cc247b2c30361c447ad499941f6a7
Mike Andrews <mja.hlv@gmail.com>
2020-03-20 16:06:52 +0000

    Added preliminary logic for auditing

--- Common/Defra.Imports.BusinessLogic/ImportApplication/RiskLevelCounterManager/AbstractRiskCounterManager.cs
+++ Common/Defra.Imports.BusinessLogic/ImportApplication/RiskLevelCounterManager/AbstractRiskCounterManager.cs
@@ -20,37 +20,73 @@
         protected IAutonumberRepository _autoNumberRepo;
         protected ILogWriter _logWriter;
 
+        public delegate void IncrementNumberDelegate(string reason);
+        public event IncrementNumberDelegate IncrementNumberEvent;
+
+        public delegate void DecrementNumberDelegate(string reason);
+        public event IncrementNumberDelegate DecrementNumberEvent;
+
+        public delegate void SetNumberDelegate(string reason, int value);
+        public event SetNumberDelegate SetNumberEvent;
+
+        public delegate void IncrementQuotaDelegate(string reason);
+        public event IncrementNumberDelegate IncrementQuotaEvent;
+
+        public delegate void DecrementQuotaDelegate(string reason);
+        public event IncrementNumberDelegate DecrementQuotaEvent;
+
+        public delegate void SetQuotaDelegate(string reason, int value);
+        public event SetQuotaDelegate SetQuotaEvent;
+
         public virtual void IncrementNumber(ref defraimp_importapplication importApplication, string reason)
         {
+            // Call the increment number event
+            IncrementNumberEvent(reason);
+
             //Broadcast Stuff
             _logWriter.Log(Severity.Info, "DetermineInspectionLogic", "Increment because: " + reason);
         }
 
         public virtual void DecrementNumber(ref defraimp_importapplication importApplication, string reason)
         {
+            // Call the decrement number event
+            DecrementNumberEvent(reason);
+
             //Broadcast Stuff
             _logWriter.Log(Severity.Info, "DetermineInspectionLogic", "Decrement because: " + reason);
         }
         public virtual void SetNumberValue(ref defraimp_importapplication importApplication, string reason, int value)
         {
+            // Call the set number event
+            SetNumberEvent(reason, value);
+
             //Broadcast Stuff
             _logWriter.Log(Severity.Info, "DetermineInspectionLogic", "Set number because: " + reason);
         }
 
         public virtual void IncrementQuota(ref defraimp_importapplication importApplication, string reason)
         {
+            // Call the increment quota event
+            IncrementQuotaEvent(reason);
+
             //Broadcast Stuff
             _logWriter.Log(Severity.Info, "DetermineInspectionLogic", "Increment Quota because: " + reason);
         }
 
         public virtual void DecrementQuota(ref defraimp_importapplication importApplication, string reason)
         {
+            // Call the decrement quota event
+            DecrementQuotaEvent(reason);
+
             //Broadcast Stuff
             _logWriter.Log(Severity.Info, "DetermineInspectionLogic", "Decrement Quota because: " + reason);
         }
 
         public virtual void SetQuotaValue(ref defraimp_importapplication importApplication, string reason, int value)
         {
+            // Call the set quota event
+            SetQuotaEvent(reason, value);
+
             //Broadcast Stuff
             _logWriter.Log(Severity.Info, "DetermineInspectionLogic", "Set Quota because: " + reason);
         }
---
+++ Common/Defra.Imports.BusinessLogic/ImportApplication/RiskLevelCounterManager/Auditor/RiskCounterAuditor.cs
@@ -0,0 +1,51 @@
+﻿using Defra.Imports.BusinessLogic.ImportApplication.Contexts;
+using Defra.Imports.BusinessLogic.ImportApplication.Factories;
+using Defra.Imports.BusinessLogic.Logging;
+using Defra.Imports.BusinessLogic.RepoInterfaces;
+using Defra.Imports.BusinessLogic.ImportApplication.DetermineInspection.Helpers;
+using Defra.Imports.BusinessLogic.ImportApplication.DetermineInspection.Strategies;
+using Defra.Imports.Model;
+using Defra.Imports.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Defra.Imports.BusinessLogic.ImportApplication.RiskLevelCounterManager.Auditor
+{
+    class RiskCounterAuditor
+    {
+        AbstractRiskCounterManager _riskLevelCounterManager;
+        ICrmRepository<defraimp_counterhistory> _counterHistoryRepo;
+
+        public RiskCounterAuditor(AbstractRiskCounterManager riskLevelCounterManager, ICrmRepository<defraimp_counterhistory> counterHistoryRepo)
+        {
+            _riskLevelCounterManager = riskLevelCounterManager;
+
+            //Subscribe to the events we care about
+            _riskLevelCounterManager.IncrementNumberEvent += IncrementNumber;
+            _riskLevelCounterManager.DecrementNumberEvent += DecrementNumber;
+            _riskLevelCounterManager.SetNumberEvent += SetNumber;
+            _riskLevelCounterManager.IncrementQuotaEvent += IncrementQuota;
+            _riskLevelCounterManager.DecrementQuotaEvent +=
+        }
+
+        void AuditCounterEvent(string reason)
+        {
+
+        }
+
+        void CreateCounterHistoryRecord()
+        {
+            defraimp_counterhistory counterHistoryRecord = new defraimp_counterhistory();
+
+            counterHistoryRecord.defraimp_CounterHistoryType = defraimp_counterhistory_defraimp_counterhistorytype.AutoNumber;
+            counterHistoryRecord.defraimp_Operation = defraimp_counterhistory_defraimp_operation.;
+            counterHistoryRecord.defraimp_AutoNumberId =;
+            counterHistoryRecord.defraimp_Reason = defraimp_counterhistory_defraimp_reason.;
+            counterHistoryRecord.defraimp_PreviousValue = 0;
+            counterHistoryRecord.defraimp_CurrentValue = 1;
+
+            _counterHistoryRepo.Create(counterHistoryRecord);
+        }
+    }
+}
