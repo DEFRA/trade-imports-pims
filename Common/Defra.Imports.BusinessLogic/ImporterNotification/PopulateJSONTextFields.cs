546a03a4af7a2bd3579ed031f8299eafb47fe504
Fernandes, Gonçalo <nuno.fernandes@capgemini.com>
2020-07-01 09:41:23 +0100

    - Removing space

--- Common/Defra.Imports.BusinessLogic/ImporterNotification/PopulateJSONTextFields.cs
+++ Common/Defra.Imports.BusinessLogic/ImporterNotification/PopulateJSONTextFields.cs
@@ -163,7 +163,6 @@ namespace Defra.Imports.BusinessLogic.ImporterNotification
         {
             var imageValue = (preImage != null) ? preImage.GetAttributeValue<string>(value) : string.Empty;
             var validValue = (target.Contains(value)) ? target.GetAttributeValue<string>(value) : imageValue;
-
             return validValue;
         }
     }
