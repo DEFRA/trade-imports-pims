bb169ac18c7efea4b418b94f86e214245c1fd78a
Fernandes, Gonçalo <nuno.fernandes@capgemini.com>
2020-07-02 16:18:02 +0100

    - Adding Plugin to pull notes and clone them, from original import query

---
+++ Solutions/DefraImportsPlugins/PluginAssemblies/Defra.Imports.Plugins/ImportQuery/CloneProcessesImportQuery.cs
@@ -0,0 +1,38 @@
+﻿using Defra.Imports.BusinessLogic;
+using Defra.Imports.BusinessLogic.Import_Query;
+using Defra.Imports.BusinessLogic.Logging;
+using Defra.Imports.Model;
+using Defra.Imports.Repositories.Annotations;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defra.Imports.Plugins.ImportQuery
+{
+    [CrmPluginRegistration(
+     MessageNameEnum.Create,
+     "defraimp_importquery",
+     StageEnum.PostOperation,
+     ExecutionModeEnum.Synchronous,
+     "defraimp_importqueryid",
+     "Create Step - Import Query Clone Processes",
+     0,
+     IsolationModeEnum.Sandbox)]
+
+    public class CloneProcessesImportQuery : Plugin
+    {
+        protected override void Execute(IPluginExecutionContext context, IOrganizationService orgSvc, TracingServiceLogWriter logWriter, RepositoryFactory repositoryFactory)
+        {
+            var crmContext = new ImportsContext(orgSvc);
+            var annotationRepos = new AnnotationsRepository(crmContext);
+
+            var importQueryFromContext = ((Entity)context.InputParameters["Target"]).ToEntity<defraimp_importquery>();
+
+            var addNotesToCloneImportQuery = new AddNotesToCloneImportQuery(annotationRepos, importQueryFromContext);
+            addNotesToCloneImportQuery.CloneNotes();
+        }
+    }
+}
