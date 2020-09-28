327812f279b4c06c8597f1e3b4ebd0d7b126de25
O'Leary, Aidan <aidan.o-leary@capgemini.com>
2020-09-28 16:43:57 +0000

    Merged PR 13173: Changed package deployer version number
    Changed package deployer version number to fix issue with release pipeline

--- Deploy/PackageTemplate.cs
+++ Deploy/PackageTemplate.cs
@@ -1,7 +1,7 @@
 namespace Defra.Imports.Deployment
 {
     using System.ComponentModel.Composition;
-    using Capgemini.PowerApps.Deployment;
+    using Capgemini.Xrm.Deployment.PackageDeployer;
     using Microsoft.Xrm.Tooling.PackageDeployment.CrmPackageExtentionBase;
 
     /// <summary>
