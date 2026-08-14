c4eaf17e1bce2a2a6fc9d8e5c27d209275bcf37f
Max Ewing <max.ewing@capgemini.com>
2026-08-14 17:59:37 +0100

    Deployed 28e2b1a with MkDocs version: 1.6.1

---
+++ scripts/mermaid.js
@@ -0,0 +1,10 @@
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "default",
+  flowchart: {
+    defaultRenderer: "elk",
+    nodeSpacing: 50,
+    rankSpacing: 80,
+    useMaxWidth: false
+  }
+});
